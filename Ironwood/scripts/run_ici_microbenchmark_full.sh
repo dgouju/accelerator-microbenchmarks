6b637d0b9db9bdab26b168903503ab5f489dd060
dgouju <dgouju@users.noreply.github.com>
2025-12-02 11:58:30 +0100

    Update run_ici_microbenchmark_full.sh

--- Ironwood/scripts/run_ici_microbenchmark_full.sh
+++ Ironwood/scripts/run_ici_microbenchmark_full.sh
@@ -8,9 +8,9 @@ BUCKET=$2
 TIMESTAMP=$(date +%y-%m-%d_%H-%M-%S)
 CONFIG_NAMES='reduce_scatter_1d reduce_scatter_2d all_gather_3d all_reduce_3d all_to_all_3d all_gather_2d all_reduce_2d all_to_all_2d all_gather_1d all_reduce_1d all_to_all_1d'
 
-apt-get update && apt-get install -y curl gnupg
+apt-get update && apt-get install -y curl gnupg apt-transport-https ca-certificates
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 apt-get update && apt-get install -y google-cloud-sdk
 
 for CONFIG in $CONFIG_NAMES
